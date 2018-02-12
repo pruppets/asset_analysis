ca5d4bded945d22bdfabe989bc8ecf87d2776d18
pruppets <david.weis.1010@gmail.com>
2018-02-12 00:35:06 -0700

    updating my own project, saving whatever i did to the other one which couldnt havent been much

--- scratch.py
+++ scratch.py
@@ -1,8 +1,11 @@
+import pandas as pd
 from pandas_datareader import data
 import datetime
 
 #demonstrating the tool that pulls from yahoo
 aapl = data.DataReader('AAPL','yahoo',start=datetime.datetime(2016, 1, 1),end=datetime.datetime.now())
+spy = data.DataReader('SPY', 'yahoo', start=datetime.datetime(2016, 1, 1), end=datetime.datetime.now())
+ibm = data.DataReader('IBM', 'yahoo', start=datetime.datetime(2016, 1, 1), end=datetime.datetime.now())
 
 #showing the columns
 aapl.dtypes
@@ -22,4 +25,11 @@ start=datetime.datetime(2018, 2, 11)
 date_delta = datetime.datetime(2018, 2, 11) - datetime.datetime(2016, 1, 1)
 date_delta.days*float(5)/7
 
+
+
 #next is printing the dates to excel.  this will help me evalauate how concatenate is working hoping that they don't perfectly share all of the indices.  does it kick out indices that don't match?
+ibm.to_csv('G:\\PyCharm Projects\\asset_analysis\\ibm.csv')
+data = pd.concat([aapl['Close'], spy['Close'], ibm['Close']], axis=1)
+data.to_csv('G:\\PyCharm Projects\\asset_analysis\\data.csv')
+
+print('for break')
