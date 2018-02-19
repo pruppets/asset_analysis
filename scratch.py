108a5c05d8fba3f836e02c6ea30ca5118bd7df0e
pruppets <david.weis.1010@gmail.com>
2018-02-19 00:27:29 -0700

    updating my own project, saving whatever i did to the other one which couldnt havent been much

--- scratch.py
+++ scratch.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas_datareader import data
 import datetime
+import qqpat
 
 #demonstrating the tool that pulls from yahoo
 aapl = data.DataReader('AAPL','yahoo',start=datetime.datetime(2016, 1, 1),end=datetime.datetime.now())
@@ -25,12 +26,17 @@ start=datetime.datetime(2018, 2, 11)
 date_delta = datetime.datetime(2018, 2, 11) - datetime.datetime(2016, 1, 1)
 date_delta.days*float(5)/7
 
-
-
 #next is printing the dates to excel.  this will help me evalauate how concatenate is working hoping that they don't perfectly share all of the indices.  does it kick out indices that don't match?
 ibm.to_csv('G:\\PyCharm Projects\\asset_analysis\\ibm.csv')
 data = pd.concat([aapl['Close'], spy['Close'], ibm['Close']], axis=1)
 data.to_csv('G:\\PyCharm Projects\\asset_analysis\\data.csv')
 
+analyzer = qqpat.Analizer(data, column_type='price', titles=["APPL", "SPY", "IBM"])
+summary = analyzer.get_statistics_summary()
 
+#is a list for each stock price column of different statistics
+type(summary)
+
+#dictionary with the statsitics for a given column (stock)
+type(summary[0])
 print('for break')
